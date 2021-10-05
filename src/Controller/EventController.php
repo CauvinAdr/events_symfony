d086541d1481e9022390ef7e142615f50818b807
Cauvin <adrien.cauvin@gmail.com>
2021-10-05 16:39:18 +0200

    Add form for event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventType;
 use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     /**
-     * @Route("/event", name="event")
+     * @Route("/events", name="event")
      */
     public function index(EventRepository $eventRepository): Response
     {
@@ -18,6 +23,38 @@ class EventController extends AbstractController
 
         return $this->render('event/index.html.twig', [
             'allEvents' => $allEvents,
+        ]);
+    }
+
+    /**
+     * @Route("/events/add", name="event_add")
+     */
+    public function add(Request $request, EntityManagerInterface $entityManager)
+    {
+        $event = new Event();
+        $addEventForm = $this->createForm(EventType::class, $event);
+
+        $addEventForm->handleRequest($request);
+
+        if($addEventForm->isSubmitted() && $addEventForm->isValid()) {
+            $event = $addEventForm->getData();
+
+            $entityManager->persist($event);
+            $entityManager->flush();
+        }
+
+        return $this->render('event/add.html.twig', [
+            'addEventForm' => $addEventForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/events/{event}", name="event_show")
+     */
+    public function show(Event $event)
+    {
+        return $this->render('event/show.html.twig', [
+            'event' => $event
         ]);
     }
 }
